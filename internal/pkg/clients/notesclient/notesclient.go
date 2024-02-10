aead049af8a359ef08f6f45aeac5a2f41d8d74b8
Pos1t1veM1ndset <swad0781@gmail.com>
2024-02-10 12:56:24 +0300

    test gofumpt&lint-1

--- internal/pkg/clients/notesclient/notesclient.go
+++ internal/pkg/clients/notesclient/notesclient.go
@@ -8,12 +8,11 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"notes/internal/pkg/config"
+	"notes/internal/pkg/models"
 	"path"
 	"strconv"
 	"time"
-
-	"notes/internal/pkg/config"
-	"notes/internal/pkg/models"
 )
 
 var ErrNotFound = errors.New("not found")
